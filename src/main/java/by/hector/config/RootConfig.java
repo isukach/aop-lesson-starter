ea2b6b4371214495328be9f2a310072f333a90e3
Igor Sukach <Igor.Sukach@kandasoft.com>
2017-07-04 23:29:20 +0300

    Initial commit

---
+++ src/main/java/by/hector/config/RootConfig.java
@@ -0,0 +1,13 @@
+package by.hector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author i.sukach
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class RootConfig {
+
+}
